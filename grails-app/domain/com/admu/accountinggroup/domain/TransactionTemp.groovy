5f46e722ce7f4649991de371036eb71deb329b71
Aldrin Tingson <aldrin@Aldrin-MBP.mshome.net>
2015-10-13 16:46:31 +0800

    adding temp folders

---
+++ grails-app/domain/com/admu/accountinggroup/domain/TransactionTemp.groovy
@@ -0,0 +1,35 @@
+package com.admu.accountinggroup.domain
+
+import com.admu.accountinggroup.Side
+
+class TransactionTemp {
+
+    Account account
+    BigDecimal amount
+    String comment
+    String description
+    Side postingKey
+    TransactionDocumentTemp transactionDocument
+
+
+    static constraints = {
+        amount nullable: false, min: BigDecimal.ZERO
+        postingKey nullable: false
+        account nullable: false
+        postingKey nullable: false
+        transactionDocument nullable: false
+    }
+
+    static belongsTo = [account : Account, transactionDocument : TransactionDocumentTemp]
+
+    static mapping = {
+        table 'transactions_temp'
+        version false
+        id type: 'long', sqlType: 'serial', generator:'sequence', params:[sequence:'transactions_temp_id_seq']
+        comment type: 'text'
+        description type : 'text'
+        account type: 'BigInteger', sqlType: 'int4'
+        transactionDocument type: 'BigInteger', sqlType: 'int4'
+        postingKey sqlType: 'bpchar', type: 'char'
+    }
+}
